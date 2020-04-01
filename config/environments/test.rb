0ba84f0a06f97d1325878507d49a5966e121b988
mariannehbt <marianne.heribert@gmail.com>
2020-04-01 14:33:45 +0200

    [CORRECT] config.action_mailer test to prod - marianne

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,7 +37,6 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => 'project-aap-covid19.herokuapp.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
