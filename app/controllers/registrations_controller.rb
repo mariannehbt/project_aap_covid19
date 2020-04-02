25756ecb9ca7a79adacc4da1453f71e77a08cabc
Emilie-thp <emilie.marjollet@orange.fr>
2020-04-02 11:58:59 +0200

    Survey for signed_in user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
   after_action :link_survey_to_user, only: :create
 
   def link_survey_to_user
-  	current_user.surveys << Survey.find(params[:user][:surveyid])
+  	if user_signed_in?
+	  	current_user.surveys << Survey.find(params[:user][:surveyid])
+	  end
   end
 
   protected
