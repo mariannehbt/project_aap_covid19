b20831443a76df0f58308bfafb9ad51c9e05d11f
mariannehbt <marianne.heribert@gmail.com>
2020-04-02 14:32:48 +0200

    [UPDATE] cmp scraper + url departements - marianne

--- lib/cmp_scrap.rb
+++ lib/cmp_scrap.rb
@@ -3,6 +3,40 @@
 require 'nokogiri'
 require 'open-uri'
 
+def url
+	
+end
+
+def open_departements_list
+	url = 'http://www.santeenfrance.fr/annuaire/13-centres-medico-psychologiques-cmp-cattp'
+	html_content = open(url).read
+	departements_list_page = Nokogiri::HTML(html_content)
+	return departements_list_page
+end
+
+def get_all_departements(dpt_list)
+  all_departements = dpt_list.xpath("//*[@id='page']/div[3]/div/div[1]/div/ul/li/a")
+  all_departements_array = []
+  all_departements.each do |each_departement|
+  	all_departements_array << (each_departement.text).split('(').last.split(')').first
+  end
+  return all_departements_array
+end
+
+def put_all_departements_urls_in_array(dpt_list_page, dpt_zip_list)
+	url = 'http://www.santeenfrance.fr/annuaire/13-centres-medico-psychologiques-cmp-cattp/dep:'
+	all_departements_urls_array = []
+	dpt_zip_list.each do |dpt_zip|
+		page = url+dpt_zip.to_s
+		# html_content = open(url+dpt_zip.to_s).read
+		# page = Nokogiri::HTML(html_content)
+		all_departements_urls_array << page
+	end
+	return all_departements_urls_array
+end
+
+###
+
 def open_departement_first_page
 	departement_first_page = Nokogiri::HTML(open('http://www.santeenfrance.fr/annuaire/13-centres-medico-psychologiques-cmp-cattp/dep:75'))
 	return departement_first_page
@@ -52,6 +86,9 @@ def create_final_array(names, addresses)
 end
 
 def perform
+	dpt_list = open_departements_list
+	dpt_all = get_all_departements(dpt_list)
+	dpt_all_array = put_all_departements_urls_in_array(dpt_list, dpt_all)
 	dpt_first_page = open_departement_first_page
 	dpt_last_page = find_departement_last_page_number(dpt_first_page)
 	dpt_all_urls_array = put_departement_all_urls_in_array(dpt_first_page, dpt_last_page)
@@ -61,14 +98,19 @@ def perform
 
 
 	puts '*' * 50
-	puts "Nombre de pages : #{dpt_last_page} (#{dpt_all_urls_array.size})"
+	# puts dpt_all.size
+	# puts dpt_all
 	puts '-' * 50
-	puts "Nombre de centres : #{dpt_all_pages_cmp_names.size}"
+	puts dpt_all_array.size
+	puts dpt_all_array
+	# puts "Nombre de pages : #{dpt_last_page} (#{dpt_all_urls_array.size})"
+	# puts '-' * 50
+	# puts "Nombre de centres : #{dpt_all_pages_cmp_names.size}"
+	# puts '-' * 50
+	# puts dpt_all_pages_cmp_names
+	# puts '-' * 50
+	# puts dpt_all_pages_cmp_addresses
 	puts '-' * 50
-	puts dpt_all_pages_cmp_names
-	puts '-' * 50
-	puts dpt_all_pages_cmp_addresses
-	puts '*' * 50
 	puts dpt_all_data
 	puts dpt_all_data.size
 	puts '*' * 50
