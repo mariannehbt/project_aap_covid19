83bb48b8acc3b2cd76418db7eed9a41d40a6383f
Emilie-thp <emilie.marjollet@orange.fr>
2020-04-02 18:37:47 +0200

    Added now attributes to cmps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_092251) do
+ActiveRecord::Schema.define(version: 2020_04_02_163551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2020_04_01_092251) do
     t.string "phonenumber"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "city"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "surveys", force: :cascade do |t|
